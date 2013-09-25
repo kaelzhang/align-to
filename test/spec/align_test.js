9b011d2801b29ee7a1d31abdfd5317dd3cffce74
Kael <i@kael.me>
2013-09-25 17:43:03 +0800

    update test fixtures to the latest version of cortex

--- test/spec/align_test.js
+++ test/spec/align_test.js
@@ -1,7 +1,7 @@
 describe("align", function(){
     describe("align.my_method()", function(){
         it("should return 1", function(done){
-            NR.use(function() {
+            use(function() {
                 
             });
         });
